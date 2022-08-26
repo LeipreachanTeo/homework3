2b74a61ec6911c7b24657cbe71e99013178c5cd3
LeipreachanTeo <pokiboom@mail.ru>
2022-08-26 19:12:46 +0700

    first task

---
+++ task_one/task_one.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Введите пятизначное число: ");
+int number = Int32.Parse(Console.ReadLine());
+int reverse = 0;
+int identical_number = number;
+while(identical_number > 0)
+{
+    reverse = reverse*10 + identical_number % 10;
+    identical_number /= 10;
+}
+if (number > 9999 && number < 100000)
+{
+    if(number == reverse)
+    {
+        Console.WriteLine("Число является палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число не является палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
