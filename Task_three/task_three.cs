472071df51a6b5d363fe9d24c0e090eb48b4e9ef
LeipreachanTeo <pokiboom@mail.ru>
2022-08-26 19:59:40 +0700

    third task

---
+++ Task_three/task_three.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int [] array = new int [N];
+int index = 0;
+int number = 1;
+
+while(index < N)
+{
+    array[index] = number * number * number;
+    Console.Write(array[index] + " ");
+    number += 1;
+    index += 1;
+}
