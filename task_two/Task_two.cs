7aa0c4598eeb876b59d9e54d8da9678ad7a95b02
LeipreachanTeo <pokiboom@mail.ru>
2022-08-26 19:39:33 +0700

    Second task

---
+++ task_two/Task_two.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты x1: ");
+int x1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты y1: ");
+int y1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты z1: ");
+int z1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты x2: ");
+int x2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты y2: ");
+int y2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты z2: ");
+int z2 = int.Parse(Console.ReadLine());
+
+var distanse = Math.Sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)) + ((z2-z1)*(z2-z1)));
+Console.WriteLine(distanse);
